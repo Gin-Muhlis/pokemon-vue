fbecbf9a3db916fa70e9f960117e0bd98e9838f1
gin-muhlis <ginxrpl07@gmail.com>
2024-10-15 11:07:49 +0700

    fix: mengatur struktur folder dan konfigurasi tailwind

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-red-500 text-center my-10 font-bold text-5xl">
+        hello world
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
